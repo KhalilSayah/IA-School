d1c90cca1b00cea867e08cb570a0b96d753f51de
Khalil <sayahkhalil2001@gmail.com>
2024-09-19 09:50:16 +0200

    visualisation recherche linaire

---
+++ scripts/visualisation_algo_recherche.py
@@ -0,0 +1,47 @@
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
+def recherche_linaire_graph(l, cible):
+    for i in range(len(l)):
+        yield i
+        if l[i] == cible:
+            return i
+        
+def visualize_recherche_linaire(l:list[int], cible:int):
+    fig,ax = plt.subplots(())
+
+    bars = ax.bar(
+        range(len(l)),
+        l,
+        color = 'blue'
+    )
+
+    def init():
+        for bar in bars:
+            bar.set_color("blue")
+        return bars
+    
+    def update(i):
+        for bar in bars:
+            bar.set_color("blue")
+        bars[i].set_color("red")
+        return bars
+    
+    ani = animation.FuncAnimation(
+        fig,
+        update,
+        frames= recherche_linaire_graph(l,cible),
+        init_func= init,
+        interval = 100,
+        repeat = False
+        
+    )
+
+    plt.show()
+
+
+random_values = [random.randint(0, 100) for _ in range(50)]
+visualize_recherche_linaire(random_values, 23)
