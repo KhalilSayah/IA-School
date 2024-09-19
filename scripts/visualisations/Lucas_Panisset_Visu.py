d387a0a4cfd1f23be0dd513172e86e852ff85458
Lucas Panisset <lucas.panisset.pro@gmail.com>
2024-09-19 10:56:46 +0200

    exo

---
+++ scripts/visualisations/Lucas_Panisset_Visu.py
@@ -0,0 +1,55 @@
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
+def rechercheBinaire(list,n):
+    g=0
+    d=len(list)-1
+    m=(g+d)//2
+    while g<=d :
+        if list[m]==n:
+                return m
+        elif list[m]>n:
+            d=m-1
+        else:
+            g=m +1
+        m=(g+d)//2
+    return d
+        
+def visualize_recherche_linaire(l:list[int], cible:int):
+    fig,ax = plt.subplots()
+
+    bars = ax.bar(
+        range(len(l)),
+        l,
+        color = 'blue'
+    )
+
+    def init():
+        for bar in bars:
+            bar.set_color("blue")
+        return bars
+    
+    def update(i):
+        for bar in bars:
+            bar.set_color("blue")
+        bars[i].set_color("red")
+        return bars
+    
+    ani = animation.FuncAnimation(
+        fig,
+        update,
+        frames= rechercheBinaire(l,cible),
+        init_func= init,
+        interval = 100,
+        repeat = False
+        
+    )
+
+    plt.show()
+
+
+random_values = [random.randint(0, 100) for _ in range(50)]
+visualize_recherche_linaire(random_values, 38)
